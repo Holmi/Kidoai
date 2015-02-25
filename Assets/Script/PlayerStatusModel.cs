f87cef165536f70124f2d5280428a90c8f9cbdc7
Mukai <rm399343@gmail.com>
2015-02-25 19:18:00 +0900

    ステータスの保持の仕方を変更

--- Assets/Script/PlayerStatusModel.cs
+++ Assets/Script/PlayerStatusModel.cs
@@ -1,39 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerStatusModel : MonoBehaviour {
+public class PlayerStatusModel {
 
-	public static int p1_money;
-	public static int p1_stamina;
-	public static int p1_looks;
-    public static int p1_maxStamina;
-    public static int p1_takeTime;
+	public static PlayerStatus player1 = new PlayerStatus();
+	public static PlayerStatus player2 = new PlayerStatus();
 
-    public static int p2_money;
-    public static int p2_stamina;
-    public static int p2_looks;
-    public static int p2_maxStamina;
-    public static int p2_takeTime;
+	public static int nowDate = 0;
 
-	// Use this for initialization
-	void Start () {
-	
+	public class PlayerStatus {
+		public PlayerStatus() {
+			money = 0;
+			maxStamina = 10;
+			stamina = maxStamina;
+			looks = 0;
+			takeTime = 0;
+		}
+
+		// プレイヤー1の所持金額値
+		private int money;
+		public int Money {
+			get { return money; }
+			set { money = (value >= 0) ? value : money; }
+		}
+
+		// 体力値
+		private int stamina;
+		public int Stamina {
+			get { return stamina; }
+			set { stamina = value; }
+		}
+
+		// ルックス値
+		private int looks;
+		public int Looks { 
+			get { return looks; }
+			set { looks = value; }
+		}
+
+		// 最大体力値
+		private int maxStamina;
+		public int MaxStamina {
+			get { return maxStamina; }
+			set { maxStamina = (value >= maxStamina) ? value : maxStamina; }
+		}
+
+		// かかる時間
+		public int takeTime;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    public static int getP1Money() { return p1_money; }
-    public static int getP1Stamina() { return p1_stamina; }
-    public static int getP1Looks() { return p1_looks; }
-    public static int getP1MaxStamina() { return p1_maxStamina; }
-    public static int getP1TakeTime() { return p1_takeTime; }
-
-    public static int getP2Money() { return p2_money; }
-    public static int getP2Stamina() { return p2_stamina; }
-    public static int getP2Looks() { return p2_looks; }
-    public static int getP2MaxStamina() { return p2_maxStamina; }
-    public static int getP2TakeTime() { return p2_takeTime; }
 }
